1ea5fe98e42900f0b5b621da2b6ca0b683e59708
XYenChi <oriachiuan@gmail.com>
2023-02-15 19:42:26 +0800

    add vmul, vdiv, vmax, vmin and vrem

--- operator_py_function.py
+++ operator_py_function.py
@@ -2,3 +2,20 @@ def add_op(a, b):
     return a + b
 def sub_op(a, b):
     return a - b
+def mul_op(a, b):
+    return a * b
+def div_op(a, b):
+    return a / b
+
+def max_op(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+def min_op(a, b):
+    if a < b:
+        return a
+    else:
+        return b
+def reminder(a, b):
+    return a % b
