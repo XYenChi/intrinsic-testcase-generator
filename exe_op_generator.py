4cf2e39345926aa6be2b6ae619ce8363a77e8c51
XYenChi <oriachiuan@gmail.com>
2023-02-23 16:52:18 +0800

    1.Initiate array as SEW 2.Vector load instruction operate the array for moving the pointer later. 3.Adjust the opetator formate to fit different parameter position.

--- exe_op_generator.py
+++ exe_op_generator.py
@@ -10,7 +10,7 @@ import math
 
 op = sys.argv[1]
 Q_array = 16
-ELEN = 64
+avl = 64
 _vx = ['v', 'x']
 _suffix = ['8m1', '8m2', '8m4', '8m8', '8mf2', '8mf4', '8mf8',
            '16m1', '16m2', '16m4', '16m8', '16mf2', '16mf4',
@@ -30,33 +30,24 @@ lmul_dict = {'1': 1, '2': 2, '4': 4, '8': 8, 'f2': 0.5, 'f4': 0.25, 'f8': 0.125}
 class Node:
     def __init__(self):
         self.op = op
+        self.op_mask = ""
+        # pass different parameter sequence
         self.lmul = 0.0
         self.sew = 8
         self.mask = None
         # If _mask = '_m', vm = 0, masked, calculate if mask array[i] = 1
         self.sign = None
-        self.dynamic_arrayQuantity_elementRange()
-        self.Q_A_E = 64
+        self.Q_A_E = 16
         self.Range = 1
-        self.masked = [0] * self.Q_A_E
-        self.data1 = [0] * self.Q_A_E
-        self.data2 = [0] * self.Q_A_E
-        self.vd_default = [0] * self.Q_A_E
-        self.out = [0] * self.Q_A_E
+        self.masked = [0] * Q_array
+        self.data1 = [0] * Q_array
+        self.data2 = [0] * Q_array
+        self.vd_default = [0] * Q_array
+        self.out = [0] * Q_array
         self.val = None
-        self.golden = [0] * self.Q_A_E
+        self.golden = [0] * Q_array
         self.random_gen()
         self.compute()
-
-    def dynamic_arrayQuantity_elementRange(self):
-        self.Q_A_E = int(128 * self.lmul / self.sew)
-        # quantity of array element
-        self.Range = pow(2, self.sew)
-        self.masked = [0] * self.Q_A_E
-        self.data1 = [0] * self.Q_A_E
-        self.data2 = [0] * self.Q_A_E
-        self.vd_default = [0] * self.Q_A_E
-        self.golden = [0] * self.Q_A_E
 
     def compiler_option_write(self):
         fd.write("/* { dg-do run } */\n")
@@ -68,34 +59,132 @@ class Node:
         fd.write("#include <string.h>\n")
         fd.write("#include \"riscv_vector.h\"\n")
 
+    def vl_set_write(self):
+        fd.write("size_t vl = vsetvl_e%s(%s);\n" % (suffix, avl))
+
     def c_main_entry_write(self):
         fd.write("int main(){\n")
 
     def pointer_iterator_write(self):
-        fd.write("    for (int n = %d, Q_element = %s;n >= 0; n -= Q_element) {\n" % (a.Q_A_E, a.sew))
+        fd.write("    for (size_t n = 0; n < vl; n++) {\n")
 
-    def intrinsic_data_write(self):
-        fd.write("data1, data2")
+    def vs2_load(self):
+        fd.write("    vint%s_t __riscv_vle%s_v_i%s (*in1, vl);\n" % (suffix, a.sew, suffix))
 
-    def intrinsic_ELEN_write(self):
-        fd.write("%s" % ELEN)
+    def vs1_load(self):
+        fd.write("    vint%s_t __riscv_vle%s_v_i%s (*in2, vl);\n" % (suffix, a.sew, suffix))
+
+    def vd_load(self):
+        fd.write("    vint%s_t __riscv_vle%s_v_i%s (*out, vl);\n" % (suffix, a.sew, suffix))
+
+    # def mask_load(self):
+    # todo: load mask
+    def vd_store(self):
+        fd.write("        vint%s_t __riscv_vse%s_v_i%s (out, out_data, vl);\n" % (suffix, a.sew, suffix))
+
+    def parameter_seq_write(self):
+        match self.op_mask:
+            case 'vadd':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vadd_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vsub':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vsub_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vmul':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vmul_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vdiv':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vdiv_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vmax':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vmax_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vmin':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vmin_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vrem':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vrem_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vadc':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, mask, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vsbc':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vsbc_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vmerge':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vmerge_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
+            case 'vmacc':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (data1, data2, vl)\n" % (suffix, op, vx, suffix,
+                                                                                               mask))
+            case 'vmacc_m':
+                fd.write(
+                    "        vint%s_t out_data = __riscv_%s_v%s_i%s%s (mask, data1, data2, vl)\n" % (suffix, op, vx,
+                                                                                                     suffix, mask))
 
     def jump_to_next_write_mask(self):
-        fd.write("        in1 += Q_element;\n")
-        fd.write("        in2 += Q_element;\n")
-        fd.write("        out += Q_element;\n")
-        fd.write("        mask += Q_element;\n")
+        fd.write("        in1 += %s;\n" % int(self.sew / 8))
+        fd.write("        in2 += %s;\n" % int(self.sew / 8))
+        fd.write("        out += %s;\n" % int(self.sew / 8))
+        fd.write("        mask += %s;\n" % int(self.sew / 8))
         fd.write("      }\n")
 
     def jump_to_next_write_wo_mask(self):
-        fd.write("        in1 += Q_element;\n")
-        fd.write("        in2 += Q_element;\n")
-        fd.write("        out += Q_element;\n")
+        fd.write("        in1 += %s;\n" % int(self.sew / 8))
+        fd.write("        in2 += %s;\n" % int(self.sew / 8))
+        fd.write("        out += %s;\n" % int(self.sew / 8))
         fd.write("      }\n")
 
     def report_write(self):
         fd.write("    int fail = 0;\n")
-        fd.write("    for (int i = 0; i < %d; i++){\n" % self.Q_A_E)
+        fd.write("    for (int i = 0; i < %d; i++){\n" % Q_array)
         fd.write("        if (golden[i] != out_data[i]) {\n")
         fd.write("            printf (\"idx=%d golden=%d out=%d\\n\", i, golden[i], out[i]);\n")
         fd.write("            fail++;\n")
@@ -111,10 +200,10 @@ class Node:
         fd.write("}\n")
 
     def random_gen(self):
-        for i in range(self.Q_A_E):
-            self.data1[i] = random.randint(0, self.Range)
-            self.data2[i] = random.randint(0, self.Range)
-            self.vd_default[i] = random.randint(0, self.Range)
+        for i in range(Q_array):
+            self.data1[i] = random.randint(0, 0xff)
+            self.data2[i] = random.randint(0, 0xff)
+            self.vd_default[i] = random.randint(0, 0xff)
 
     def mask_gen(self):
         for i in range(self.Q_A_E):
@@ -126,36 +215,37 @@ class Node:
                 self.masked[i] = random.randint(0, 1)
 
     def vs2_data_write(self):
-        fd.write("    vint%s_t data1[] = {\n" % suffix)
+        fd.write("    const int%s_t data1[] = {\n" % self.sew)
         fd.write("    %s\n" % ", ".join(map(lambda x: str(x), self.data1)))
         fd.write("    };\n")
-        fd.write("    const vint%s_t *in1 = &data1[0];\n" % suffix)
+        fd.write("    const int%s_t *in1 = &data1[0];\n" % self.sew)
 
     def vs1_data_write(self):
-        fd.write("    vint%s_t data2[] = {\n" % suffix)
+        fd.write("    const int%s_t data2[] = {\n" % self.sew)
         fd.write("    %s\n" % ", ".join(map(lambda x: str(x), self.data2)))
         fd.write("    };\n")
-        fd.write("    const vint%s_t *in2 = &data2[0];\n" % suffix)
+        fd.write("    const int%s_t *in2 = &data2[0];\n" % self.sew)
 
     def vd_declaration_write(self):
-        fd.write("    vint%s_t out_data[%s];\n" % (suffix, ELEN))
-        fd.write("    vint%s_t *out = &out_data[0];\n" % suffix)
+        fd.write("    const int%s_t out_data[%s];\n" % (self.sew, Q_array))
+        # Declare the array of 10 elements
+        fd.write("    int%s_t *out = &out_data[0];\n" % self.sew)
 
     def vd_default_write(self):
-        fd.write("    vint%s_t out_data[] = {\n" % suffix)
+        fd.write("    const int out_data[] = {\n")
         fd.write("    %s\n" % ", ".join(map(lambda x: str(x), self.vd_default)))
         fd.write("    };\n")
-        fd.write("    vint%s_t *out = &out_data[0];\n" % suffix)
+        fd.write("    int%s_t *out = &out_data[0];\n" % self.sew)
 
     def mask_data_write(self):
         # todo: mask type
-        fd.write("    int masked[] = {\n")
+        fd.write("    bool%s_t masked[] = {\n" % int(self.sew / self.lmul))
         fd.write("    %s\n" % ", ".join(map(lambda x: str(x), self.masked)))
         fd.write("    };\n")
         fd.write("    const int *mask = &masked[0];\n")
 
     def golden_by_python_write(self):
-        fd.write("    vint%s_t golden[] = {\n" % suffix)
+        fd.write("    int%s_t golden[] = {\n" % self.sew)
         fd.write("    %s\n" % ", ".join(map(lambda x: str(x), self.golden)))
         fd.write("    };\n")
 
@@ -182,6 +272,8 @@ class Node:
                     fd.write("        vint%s_t out = __riscv_%s_v%s_i%s%s (" % (suffix, op, vx, suffix, mask))
                 case 'vmerge':
                     fd.write("        vint%s_t out = __riscv_%s_vv%s_i%s%s (" % (suffix, op, vx, suffix, mask))
+                case 'vmacc':
+                    fd.write("        vint%s_t out = __riscv_%s_vv%s_i%s%s (" % (suffix, op, vx, suffix, mask))
         # todo: generate all the operate by default
         # else:
         #    for op in IntegerOpList:
@@ -199,18 +291,25 @@ class Node:
             "vrem": operator_py_function.reminder,
             "vadc": operator_py_function.add_with_carry_op,
             "vsbc": operator_py_function.sub_with_borrow_op,
-            "vmerge": operator_py_function.merge_op
+            "vmerge": operator_py_function.merge_op,
+            "vmacc": operator_py_function.multiply_add_op
         }
         if self.mask:
             for i in range(self.Q_A_E):
-                self.golden[i] = op_list[op](self.data1[i], self.data2[i])
+                if op == 'vmacc':
+                    self.golden[i] = op_list[op](self.vd_default[i], self.data1[i], self.data2[i])
+                else:
+                    self.golden[i] = op_list[op](self.data1[i], self.data2[i])
         else:
             if op == 'vmerge':
                 for i in range(self.Q_A_E):
                     self.golden[i] = op_list[op](self.data1[i], self.data2[i], self.masked[i])
             else:
                 for i in range(self.Q_A_E):
-                    self.golden[i] = op_list[op](self.data1[i], self.data2[i], self.vd_default[i], self.masked[i])
+                    if op == 'vmacc':
+                        self.golden[i] = op_list[op](self.mask[i], self.vd_default[i], self.data1[i], self.data2[i])
+                    else:
+                        self.golden[i] = op_list[op](self.data1[i], self.data2[i], self.vd_default[i], self.masked[i])
 
 
 a = Node()
@@ -219,10 +318,10 @@ for suffix in _suffix:
     divider = suffix.split('m')
     a.sew = int(divider[0])
     a.lmul = lmul_dict["%s" % divider[1]]
-    a.dynamic_arrayQuantity_elementRange()
     for vx in _vx:
         for mask in _mask:
             filename = "testcase/%s_v%s_i%s%s.c" % (op, vx, suffix, mask)
+            a.op_mask = "%s%s" % (op, mask)
             if mask != '_m':
                 with open(filename, 'w') as fd:
                     if op == 'vmerge':
@@ -237,11 +336,9 @@ for suffix in _suffix:
                         a.mask_gen()
                         a.mask_data_write()
                         a.pointer_iterator_write()
-                        a.specific_operator_c()
-                        a.intrinsic_data_write()
-                        fd.write(", mask, ")
-                        a.intrinsic_ELEN_write()
-                        fd.write(");\n")
+                        # a.specific_operator_c()
+                        a.parameter_seq_write()
+                        a.vd_store()
                         a.jump_to_next_write_mask()
                         a.compute()
                         a.golden_by_python_write()
@@ -256,12 +353,15 @@ for suffix in _suffix:
                         a.vs2_data_write()
                         a.vs1_data_write()
                         a.vd_declaration_write()
+                        a.vs2_load()
+                        a.vs1_load()
+                        a.vd_load()
+                        if op == 'vmacc':
+                            a.vd_default_write()
                         a.pointer_iterator_write()
-                        a.specific_operator_c()
-                        a.intrinsic_data_write()
-                        fd.write(", ")
-                        a.intrinsic_ELEN_write()
-                        fd.write(");\n")
+                        # a.specific_operator_c()
+                        a.parameter_seq_write()
+                        a.vd_store()
                         a.jump_to_next_write_wo_mask()
                         a.compute()
                         a.golden_by_python_write()
@@ -284,11 +384,9 @@ for suffix in _suffix:
                         a.mask_gen()
                         a.mask_data_write()
                         a.pointer_iterator_write()
-                        a.specific_operator_c()
-                        fd.write(", mask, ")
-                        a.intrinsic_data_write()
-                        a.intrinsic_ELEN_write()
-                        fd.write(");\n")
+                        # a.specific_operator_c()
+                        a.parameter_seq_write()
+                        a.vd_store()
                         a.jump_to_next_write_mask()
                         a.compute()
                         a.golden_by_python_write()
