43d01ba042138902441d19798f9b1b735f89b163
XYenChi <oriachiuan@gmail.com>
2023-02-27 13:55:11 +0800

    Edit intrinsic funtion parameter type

--- testcase/vadd_vv_i8m1.c
+++ testcase/vadd_vv_i8m1.c
@@ -6,28 +6,28 @@
 #include "riscv_vector.h"
 int main(){
     const int8_t data1[] = {
-    84, 116, 40, 127, 246, 74, 218, 62, 201, 156, 247, 248, 200, 168, 139, 143
+    120, 122, 72, 30, 22, 54, 252, 87, 158, 173, 78, 61, 156, 232, 218, 35
     };
     const int8_t *in1 = &data1[0];
     const int8_t data2[] = {
-    89, 31, 215, 13, 36, 133, 0, 177, 66, 214, 166, 25, 57, 72, 147, 118
+    254, 188, 77, 149, 227, 234, 154, 70, 1, 73, 167, 222, 74, 167, 82, 155
     };
     const int8_t *in2 = &data2[0];
-    size_t vl = vsetvl_e8m1(64);
+    size_t vl = vsetvl_e8m1();
     const int8_t out_data[16];
     const int8_t *out = &out_data[0];
     vint8m1_t __riscv_vle8_v_i8m1 (const int8_t *in1, size_t vl);
     vint8m1_t __riscv_vle8_v_i8m1 (const int8_t *in2, size_t vl);
     vint8m1_t __riscv_vle8_v_i8m1 (const int8_t *out, size_t vl);
     for (size_t n = 0; n < vl; n++) {
-        vint8m1_t out_data = __riscv_vadd_vv_i8m1 (data1, data2, vl);
+        vint8m1_t out_data = __riscv_vadd_vv_i8m1 (const int8_t *in1, const int8_t *in2, size_t vl);
         vint8m1_t __riscv_vse8_v_i8m1 (*out, out_data, vl);
         in1 += 1;
         in2 += 1;
         out += 1;
       }
     int8_t golden[] = {
-    173, 147, 255, 140, 282, 207, 218, 239, 267, 370, 413, 273, 257, 240, 286, 261
+    374, 310, 149, 179, 249, 288, 406, 157, 159, 246, 245, 283, 230, 399, 300, 190
     };
     int fail = 0;
     for (int i = 0; i < 16; i++){
