0177443aa7e09b628058eeb3d453e879b7e4a2e8
XYenChi <oriachiuan@gmail.com>
2023-04-26 18:04:08 +0800

    Seperate operate with attribute

--- collector.py
+++ collector.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 import os
+import filter
+import sys
 
 filelist = set()
 location = './rvv-intrinsic-doc/auto-generated/intrinsic_funcs/'
@@ -19,12 +22,19 @@ special_set = set()
 operand_type_lists = set()
 sign = set()
 suffix = set()
+instance_operand_type_lists = set()
+instance_sign = set()
+instance_suffix = set()
+op_instance_list = list()
 for file in filelist:
     temp = str(file)
     sn = temp.split('_')
-    if sn[0] != '00' and sn[0] != '01' and sn[0] != '08' and sn[0] != '09' and sn[0] != '10' and sn[0] != '11':
+    file_to_test = sys.argv[1]
+    if sn[0] == file_to_test:
         with open(os.path.join(location, file)) as f:
+            counter = 0
             for line in f:
+                counter += 1
                 if '__riscv_' not in line:
                     continue
                 return_type_parser = line.split('__riscv_')
@@ -33,6 +43,7 @@ for file in filelist:
                 a = operand_type_parser[0][0]
                 b = operand_type_parser[0]
                 divided_suffix = b.split(a, 1)
+                # todo: "_m"
                 if divided_suffix[1] == '':
                     special_set.add(op_parser[0])
                 elif op_parser[1] == 'rtz':
@@ -40,12 +51,45 @@ for file in filelist:
                 elif divided_suffix[1] == 'u':
                     special_set.add(op_parser[0])
                 else:
+                    operand_type_lists_counter1 = len(operand_type_lists)
                     operand_type_lists.add(op_parser[1])
+                    operand_type_lists_counter2 = len(operand_type_lists)
+                    sign_counter1 = len(sign)
                     sign.add(op_parser[2][0])
+                    sign_counter2 = len(sign)
+                    function_counter1 = len(function_set)
                     function_set.add(op_parser[0])
+                    function_counter2 = len(function_set)
+                    suffix_counter1 = len(suffix)
                     suffix.add(divided_suffix[1])
-print(function_set)
-print(operand_type_lists)
-print(sign)
-print(suffix)
-print(special_set)
+                    suffix_counter2 = len(suffix)
+                    if function_counter1 != function_counter2:
+                        instance_sign = set()
+                        instance_operand_type_lists = set()
+                        instance_suffix = set()
+                        instance_name = set()
+                    elif (operand_type_lists_counter1 == operand_type_lists_counter2) & (sign_counter1 == sign_counter2) \
+                            & (function_counter1 == function_counter2) & (suffix_counter1 == suffix_counter2):
+                        instance_sign.add(op_parser[2][0])
+                        instance_operand_type_lists.add(op_parser[1])
+                        instance_suffix.add(divided_suffix[1])
+                    else:
+                        continue
+                    if (function_counter1 != function_counter2) & (counter == 1):
+                        continue
+                    elif function_counter1 != function_counter2:
+                        x = filter.Index()
+                        x.op_name = op_parser[0]
+                        x.sign_type = instance_sign
+                        x.operand_type = instance_operand_type_lists
+                        x.suffix_list = instance_suffix
+                        op_instance_list.append(x)
+generate_file = open("collector_product.txt", "w")
+generate_file.write("function_set:\n" + str(function_set) + "\n")
+generate_file.write("operand_type_lists:\n" + str(operand_type_lists) + "\n")
+generate_file.write("sign:\n" + str(sign) + "\n")
+generate_file.write("suffix:\n" + str(suffix) + "\n")
+generate_file.write("special_set:\n" + str(special_set) + "\n")
+for i in op_instance_list:
+    generate_file.write(f"function name:\n{i.op_name}\noperand type:\n{i.operand_type}\nsign type:\n{i.sign_type}\n"
+                        f"suffix list:\n{i.suffix_list}\n")
--- filter.py
+++ filter.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
-import os
-
-import exe_op_generator
-
-filelist = set()
-for files in os.walk("./rvv-intrinsic-doc/auto-generated/intrinsic_funcs/", topdown=False):
-    for name in files:
-        filelist.add(os.path.join(name))
-
-
 class Index:
     def __init__(self):
-        self.operand_num = None
+        self.op_name = ''
+        self.operand_num = set()
         # 1 for only vs2,
-        self.operand_type = None
+        self.operand_type = set()
         # v, x, w
-        self.mask_type = None
+        self.mask_type = set()
         # '', '_m', middle 'm'
-        self.sign_type = None
+        self.sign_type = set()
         # fixed i or u or both i and u
-        self.suffix_type = None
+        self.suffix_list = set()
+        self.suffix_type = ''
         # normal, narrow or widen
-    def thrower(self):
-        if self.suffix_type == 'normal':
-            if self.sign_type == 'iu':
-                if self.mask == 'normal':
-                    if self.operand_type == 'vx':
-                        exe_op_generator.GeneralFormatOpList.append(op)
-
-
 
