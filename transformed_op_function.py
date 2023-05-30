98a7a226f378238d1012dae9f063f16685e273ac
XYenChi <oriachiuan@gmail.com>
2023-05-30 18:01:09 +0800

    Add some function except float and reduce. Note that load and store function don't need to deal with now

--- transformed_op_function.py
+++ transformed_op_function.py
@@ -249,42 +249,193 @@ def vminu_op(vs2, vs1, vd=None, m=None):
         else:
             return vs1
 
+# todo: need vs2.mask[i] and vs1.mask[i]
 def vmnand_op():
-
 def vmnor_op():
 def vmor_op():
 def vmorn_op():
-def vmsbc_op():
+
+def vmsbc_op(vs2, vs1, vd, m):
+    # Produce borrow out in mask register format
+    if m == 0:
+        if vs2 >= vs1:
+            vd = 0
+            return vd
+        else:
+            vd = 1
+            return vd
+    else:
+        if vs2 >= (vs1 - 1):
+            vd = 0
+            return vd
+        else:
+            vd = 1
+            return vd
 def vmsbf_op():
-def vmseq_op():
-def vmsgt_op():
-def vmsgtu_op():
+def vmseq_op(vs2, vs1, n, m=None):
+    # vmseq
+    # n present the vd.mask
+    if m == 0:
+        return n
+    else:
+        if vs2 == vs1:
+            return 1
+        else:
+            return 0
+
+def vmsgt_op(vs2, vs1, vd, m=None):
+    if m == 0:
+        return vd
+    else:
+        if vs2 > vs1:
+            vd = 1
+            return vd
+        else:
+            vd = 0
+            return vd
+def vmsgtu_op(vs2, vs1, vd, m=None):
+    if m == 0:
+        return vd
+    else:
+        if vs2 > vs1:
+            vd = 1
+            return vd
+        else:
+            vd = 0
+            return vd
 def vmsif_op():
-def vmsle_op():
-def vmsleu_op():
-def vmslt_op():
-def vmsltu_op():
-def vmsne_op():
+def vmsle_op(vs2, vs1, vd, m=None):
+    # vmsle
+    if m == 0:
+        return vd
+    else:
+        if vs2 <= vs1:
+            vd = 1
+            return vd
+        else:
+            vd = 0
+            return vd
+def vmsleu_op(vs2, vs1, vd, m=None):
+    # vmsleu
+    if m == 0:
+        return vd
+    else:
+        if vs2 <= vs1:
+            vd = 1
+            return vd
+        else:
+            vd = 0
+            return vd
+def vmslt_op(vs2, vs1, vd, m=None):
+    # vmslt
+    if m == 0:
+        return vd
+    else:
+        if vs2 < vs1:
+            vd = 1
+            return vd
+        else:
+            vd = 0
+            return vd
+def vmsltu_op(vs2, vs1, vd, m=None):
+    # vmsltu
+    if m == 0:
+        return vd
+    else:
+        if vs2 <= vs1:
+            vd = 1
+            return vd
+        else:
+            vd = 0
+            return vd
+def vmsne_op(vs2, vs1, vd, m=None):
+    # vmsne
+    if m == 0:
+        return vd
+    else:
+        if vs2 != vs1:
+            vd = 1
+            return vd
+        else:
+            vd = 0
+            return vd
 def vmsof_op():
-def vmul_op():
-def vmulh_op():
-def vmulhsu_op():
-def vmulhu_op():
-def vmv_op():
+def vmul_op(vs2, vs1, vd=None, m=None):
+    # vmul
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1
+def vmulh_op(vs2, vs1, vd=None, m=None):
+    # vmul
+    # resolved operand width issue at the exe_op_generator
+    # todo: optimize filter method
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1
+def vmulhsu_op(vs2, vs1, vd=None, m=None):
+    # vmul
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1
+def vmulhu_op(vs2, vs1, vd=None, m=None):
+    # vmul
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1
+def vmv_op(vs2):
+    # vmv
+    return vs2
 def vmv1r_op():
 def vmv2r_op():
 def vmv4r_op():
 def vmv8r_op():
 def vmvnfr_op():
 def vmxnor_op():
+    # need vs2.mask[i] and vs1.mask[i]
 def vmxor_op():
+    # need vs2.mask[i] and vs1.mask[i]
 def vnclip_op():
 def vnclipu_op():
-def vnmsac_op():
-def vnmsub_op():
-def vnsra_op():
-def vnsrl_op():
-def vor_op():
+def vnmsac_op(vs2, vs1, vd, m=None):
+    # vnmsac
+    # Integer multiply-sub, overwrite minuend
+    if m == 0:
+        return vd
+    else:
+        vd = -(vs2 * vs1) + vd
+        return vd
+def vnmsub_op(vs2, vs1, vd, m=None):
+    # vnmsub
+    # vd[i] = -(vs1[i] * vd[i]) + vs2[i]
+    if m == 0:
+        return vd
+    else:
+        vd = -(vs1 * vd) + vs2
+        return vd
+def vnsra_op(vs2, vs1, sew, vd, m=None):
+    if m == 0:
+        return vd
+    else:
+        vd = vs2 >> (vs1 & (sew * 2 - 1))
+        return vd
+def vnsrl_op(vs2, vs1, sew, vd, m=None):
+    # operand type should be unsign
+    if m == 0:
+        return vd
+    else:
+        vd = vs2 >> (vs1 & (sew * 2 - 1))
+        return vd
+def vor_op(vs2, vs1, vd, m=None):
+    # vor
+    if m == 0:
+        return vd
+    else:
+        vd = vs1 | vs2
+        return vd
 def vredand_op():
 def vredmax_op():
 def vredmaxu_op():
@@ -293,64 +444,189 @@ def vredminu_op():
 def vredor_op():
 def vredsum_op():
 def vredxor_op():
-def vrem_op():
-def vremu_op():
+def vrem_op(vs2, vs1, vd=None, m=None):
+    if m == 0:
+        return vd
+    else:
+        if vs1 == 0:
+            return vs2
+        else:
+            return vs2 % vs1
+def vremu_op(vs2, vs1, vd=None, m=None):
+    if m == 0:
+        return vd
+    else:
+        if vs1 == 0:
+            return vs2
+        else:
+            return vs2 % vs1
 def vrgather_op():
 def vrgatherei16_op():
-def vrsub_op():
-def vs1r_op():
-def vs2r_op():
-def vs4r_op():
-def vs8r_op():
+def vrsub_op(vs2, vs1, vd=None, m=None):
+    # vrsub
+    if m == 0:
+        return vd
+    else:
+        return vs1 - vs2
+
+# don't deal with load and store function now
+# def vs1r_op():
+# def vs2r_op():
+# def vs4r_op():
+# def vs8r_op():
 def vsadd_op():
 def vsaddu_op():
-def vsbc_op():
-def vse16_op():
-def vse32_op():
-def vse64_op():
-def vse8_op():
-def vsetivli_op():
-def vsetvl_op():
-def vsetvli_op():
-def vsext_op():
+def vsbc_op(vs2, vs1, c=None, m=None):
+    if m == 1:
+        return vs2 - vs1 - m
+    else:
+        return vs2 - vs1
+
+# don't deal with load and store now
+# def vse16_op():
+# def vse32_op():
+# def vse64_op():
+# def vse8_op():
+
+# set avl = 64 bit now
+# def vsetivli_op():
+# def vsetvl_op():
+# def vsetvli_op():
+
+
+# don't need sign extension python golden function to calculate now
+# def vsext_op():
 def vslide1down_op():
 def vslide1up_op():
 def vslidedown_op():
 def vslideup_op():
-def vsll_op():
-def vsm_op():
+def vsll_op(vs2, vs1, vd, sew, m=None):
+    # vsll
+    if m == 0:
+        return vd
+    else:
+        vd = vs2 << (vs1 & (sew - 1))
+        return vd
+
 def vsmul_op():
-def vsoxei16_op():
-def vsoxei32_op():
-def vsoxei64_op():
-def vsoxei8_op():
-def vsra_op():
-def vsrl_op():
-def vsse16_op():
-def vsse32_op():
-def vsse64_op():
-def vsse8_op():
+
+# don't deal with load and store now
+# def vsm_op():
+# def vsoxei16_op():
+# def vsoxei32_op():
+# def vsoxei64_op():
+# def vsoxei8_op():
+def vsra_op(vs2, vs1, vd, sew, m=None):
+    # vsra
+    if m == 0:
+        return vd
+    else:
+        vd = vs2 >> (vs1 & (sew - 1))
+        return vd
+def vsrl_op(vs2, vs1, vd, sew, m=None):
+    # vsrl
+    if m == 0:
+        return vd
+    else:
+        vd = vs2 >> (vs1 & (sew - 1))
+        return vd
+
+# don't deal with load and store now
+# def vsse16_op():
+# def vsse32_op():
+# def vsse64_op():
+# def vsse8_op():
 def vssra_op():
 def vssrl_op():
 def vssub_op():
 def vssubu_op():
-def vsub_op():
-def vsuxei16_op():
-def vsuxei32_op():
-def vsuxei64_op():
-def vsuxei8_op():
-def vwadd_op():
-def vwaddu_op():
-def vwmacc_op():
-def vwmaccsu_op():
-def vwmaccu_op():
-def vwmaccus_op():
-def vwmul_op():
-def vwmulsu_op():
-def vwmulu_op():
+def vsub_op(vs2, vs1, vd=None, m=None):
+    # vsub
+    if m == 0:
+        return vd
+    else:
+        return vs2 - vs1
+
+# don't deal with load and store now
+# def vsuxei16_op():
+# def vsuxei32_op():
+# def vsuxei64_op():
+# def vsuxei8_op():
+def vwadd_op(vs2, vs1, vd=None, m=None):
+    # vadd vwadd
+    if m == 0:
+        return vd
+    else:
+        return vs2 + vs1
+def vwaddu_op(vs2, vs1, vd=None, m=None):
+    # vadd vwadd
+    if m == 0:
+        return vd
+    else:
+        return vs2 + vs1
+def vwmacc_op(vs2, vs1, vd=None, m=None):
+    # vmacc
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1 + vd
+def vwmaccsu_op(vs2, vs1, vd=None, m=None):
+    # vmacc
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1 + vd
+def vwmaccu_op(vs2, vs1, vd=None, m=None):
+    # vmacc
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1 + vd
+def vwmaccus_op(vs2, vs1, vd=None, m=None):
+    # vmacc
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1 + vd
+def vwmul_op(vs2, vs1, vd=None, m=None):
+    # vmul
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1
+def vwmulsu_op(vs2, vs1, vd=None, m=None):
+    # vmul
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1
+def vwmulu_op(vs2, vs1, vd=None, m=None):
+    # vmul
+    if m == 0:
+        return vd
+    else:
+        return vs2 * vs1
 def vwredsum_op():
 def vwredsumu_op():
-def vwsub_op():
-def vwsubu_op():
-def vxor_op():
-def vzext_op():
+def vwsub_op(vs2, vs1, vd=None, m=None):
+    # vsub
+    if m == 0:
+        return vd
+    else:
+        return vs2 - vs1
+def vwsubu_op(vs2, vs1, vd=None, m=None):
+    # vsub
+    if m == 0:
+        return vd
+    else:
+        return vs2 - vs1
+def vxor_op(vs2, vs1, vd=None, m=None):
+    if m == 0:
+        return vd
+    else:
+        vd = vs1 ^ vs2
+        return vd
+
+# def vzext_op():
+# calculated at the exe_op_generator, don't need now
+# todo: optimize the exe_op_generator
